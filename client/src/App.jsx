fb5f87e2066ef8c1316a6d1061ec6c39355f4da8
dhwajsharma <dhwajsharma10@gmail.com>
2022-02-18 10:03:14 +0530

    Implemented stripe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,12 +10,10 @@ import { Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/pay" element={<Pay />} />
-        <Route path="/success" element={<Success />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/pay" element={<Pay />} />
+      <Route path="/success" element={<Success />} />
+    </Routes>
   );
 };
 
--- client/src/components/Pay.jsx
+++ client/src/components/Pay.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const KEY =
   "pk_test_51KUNd1SIR2S5MwlY2PI175uJscQhb46ir7dBB4ld2CNC4PrQEiHxE1ke0XfJ9Ww3mJULAyD8ze57UihpQKqsl64D00y3YAcR7S";
 
 const Pay = () => {
+  const [stripeToken, setStripeToken] = useState(null);
+  let navigate = useNavigate();
+
   const onToken = (token) => {
-    console.log(token);
+    setStripeToken(token);
   };
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await axios.post(
+          "http://localhost:5000/api/checkout/payment",
+          {
+            tokenId: stripeToken.id,
+            amount: 2000,
+          }
+        );
+        console.log(res.data);
+        navigate("/success");
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    stripeToken && makeRequest();
+  }, [stripeToken, navigate]);
 
   return (
     <div
@@ -18,31 +42,35 @@ const Pay = () => {
         justifyContent: "center",
       }}
     >
-      <StripeCheckout
-        name="Dhwaj Shop"
-        image="https://i.guim.co.uk/img/media/b6b21223f7a169e37653d998d9541985815388b1/4_0_2559_1537/master/2559.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=5cf76ad9a55a1d0f5ce3771f5e9cf8c6"
-        billingAddress
-        shippingAddress
-        description="Your total is $20"
-        amount={2000}
-        token={onToken}
-        stripeKey={KEY}
-      >
-        <button
-          style={{
-            border: "none",
-            width: 120,
-            borderRadius: 5,
-            padding: "20px",
-            backgroundColor: "black",
-            color: "white",
-            fontWeight: "600",
-            cursor: "pointer",
-          }}
+      {stripeToken ? (
+        <span>Processing. Please wait...</span>
+      ) : (
+        <StripeCheckout
+          name="Dhwaj Shop"
+          image="https://i.guim.co.uk/img/media/b6b21223f7a169e37653d998d9541985815388b1/4_0_2559_1537/master/2559.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=5cf76ad9a55a1d0f5ce3771f5e9cf8c6"
+          billingAddress
+          shippingAddress
+          description="Your total is $20"
+          amount={2000}
+          token={onToken}
+          stripeKey={KEY}
         >
-          Pay Now
-        </button>
-      </StripeCheckout>
+          <button
+            style={{
+              border: "none",
+              width: 120,
+              borderRadius: 5,
+              padding: "20px",
+              backgroundColor: "black",
+              color: "white",
+              fontWeight: "600",
+              cursor: "pointer",
+            }}
+          >
+            Pay Now
+          </button>
+        </StripeCheckout>
+      )}
     </div>
   );
 };
